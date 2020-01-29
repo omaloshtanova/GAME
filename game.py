e95bc4e7b4c1d15bc5d276a1715286e8a8a63d80
olya <omaloshtanova@yandex.ru>
2020-01-29 23:14:20 +0700

    game

--- game.py
+++ game.py
@@ -1,30 +1,17 @@
 from os import path
-from main import HEIGHT
+from main import *
 import pygame
 import random
 
-DATA_PATH = "data"
-ROAD_IMAGE = "road.png"
-LINE_IMAGE = "line.png"
-CAR_IMAGE = "car.png"
-CAR_SOUND = "car.wav"
-CAR_CRASH_SOUND = "crush.mp3"
-WHITE_BLOCK_IMAGE = "white_block.png"
-BLACK_BLOCK_IMAGE = "black_block.png"
 
-BLOCK_IMAGES = [WHITE_BLOCK_IMAGE, BLACK_BLOCK_IMAGE]
-BLOCK_MIN = 3
-BLOCK_MAX = 6
-
-
-def load_image(name, colorkey=None):
+def load_image(name, color_key=None):
     fullname = path.join(DATA_PATH, name)
     image = pygame.image.load(fullname)
 
-    if colorkey is not None:
-        if colorkey == -1:
-            colorkey = image.get_at((0, 0))
-        image.set_colorkey(colorkey)
+    if color_key is not None:
+        if color_key == -1:
+            color_key = image.get_at((0, 0))
+        image.set_colorkey(color_key)
     else:
         image = image.convert_alpha()
 
@@ -44,11 +31,72 @@ def play_mp3(name):
     pygame.mixer.music.play()
 
 
+def print_stat(surface, time, distance, score, x=20, y=20, color=(255, 255, 255), bg_color=(0, 0, 0)):
+    pygame.draw.rect(surface, bg_color, (x, y, 205, 100))
+    font = pygame.font.SysFont(None, 30)
+    time_text = font.render(f"Время гонки: {int(time)}", 1, color)
+    surface.blit(time_text, (x + 15, y + 10))
+    distance_text = font.render(f"Расстояние: {int(distance)}", 1, color)
+    surface.blit(distance_text, (x + 15, y + 40))
+    score_text = font.render(f"Рекорд: {score}", 1, color)
+    surface.blit(score_text, (x + 15, y + 70))
+
+
+def level_passed(surface, success):
+    if success:
+        header = 'Уровень пройден'
+        message = 'для продолжения нажмите пробел'
+    else:
+        header = 'Игра окончена'
+        message = 'для выхода нажмите пробел'
+
+    header_font = pygame.font.Font(None, 80)
+    message_font = pygame.font.Font(None, 40)
+    header_text = header_font.render(header, 1, (255, 255, 255))
+    message_text = message_font.render(message, 1, (255, 255, 255))
+
+    window_width = 600
+    window_height = 250
+    window_x = WIDTH // 2 - window_width // 2
+    window_y = HEIGHT // 2 - window_height // 2
+    window_border_shift = 30
+    header_text_x = WIDTH // 2 - header_text.get_width() // 2
+    message_text_x = WIDTH // 2 - message_text.get_width() // 2
+
+    pygame.draw.rect(surface, (0, 0, 0),
+                     (window_x, window_y, window_width, window_height))
+    pygame.draw.rect(surface, (255, 255, 255),
+                     (window_x + window_border_shift, window_y + window_border_shift, window_width
+                      - window_border_shift * 2, window_height - window_border_shift * 2), 3)
+    surface.blit(header_text, (header_text_x, window_y + 60))
+    surface.blit(message_text, (message_text_x, window_y + 150))
+
+
+def build_road_object(group, current_distance, available_time):
+    if current_distance - ObjectBuilder.last_distance > ObjectBuilder.barrier_distance_min:
+        bonus_time = RACE_TIME * 2 - available_time
+        max = int(1000 * (1 - MONEY_INCIDENCE))
+        choice = random.randint(0, max)
+
+        if bonus_time > 0 and choice == 0:
+            ObjectBuilder.last_distance += ObjectBuilder.money_distance_min
+            ObjectBuilder.build_money(group)
+        else:
+            max = int(1000 * (1 - BARRIER_INCIDENCE))
+            choice = random.randint(0, max)
+
+            if choice in [0, max]:
+                ObjectBuilder.last_distance = current_distance
+                ObjectBuilder.build_barrier(group)
+
+
 class Car:
-    speedup = 50
+    timeup = 10
+    speedup = 25
     speedup_time = 1000  # 1 sec
 
-    def __init__(self):
+    def __init__(self, group):
+        self.group = group
         self.image = load_image(CAR_IMAGE)
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
@@ -59,13 +107,29 @@ class Car:
         self.speed = Car.speedup
         self.speedup_time = 0
         self.speed_sound = load_sound(CAR_SOUND)
+        self.available_time = RACE_TIME
+        self.distance = 0
 
     def draw(self, surface):
         surface.blit(self.image, self.rect)
 
-    def update(self):
+    def update(self, time):
+        self.crush_check()
+
         if self.running:
             self.rect = self.rect.move(self.direct_x, 0)
+
+            self.distance += 1
+            self.speedup_time += time
+
+            if self.speedup_time > Car.speedup_time:
+                self.speed += Car.speedup
+                self.speedup_time = 0
+
+                # Чтобы величина скорости росла, но не уходила в бесконечность
+                correct_speed = pygame.time.Clock().get_fps()
+                if self.speed - 500 > correct_speed:
+                    self.speed = correct_speed + 500
 
     def move(self, event):
         if event.type == pygame.KEYUP:
@@ -76,18 +140,26 @@ class Car:
             elif event.key == 276:
                 self.direct_x = -1
             elif event.key == 273:
-                self.running = True
-                self.speed_sound.play(loops=-1, fade_ms=10000)
+                self.run()
 
-    def get_speed(self, time):
-        if self.running:
-            self.speedup_time += time
+    def run(self):
+        self.running = True
+        self.speed_sound.play(loops=-1, fade_ms=10000)
 
-            if self.speedup_time > Car.speedup_time:
-                self.speed += Car.speedup
-                self.speedup_time = 0
+    def is_running(self):
+        return self.running
 
+    def get_distance(self):
+        return self.distance
+
+    def get_speed(self):
         return self.speed
+
+    def get_available_time(self):
+        return self.available_time
+
+    def add_time(self):
+        self.available_time += Car.timeup
 
     def stop(self):
         self.running = False
@@ -95,7 +167,21 @@ class Car:
         self.speedup_time = 0
         self.speed_sound.stop()
 
-    def side_crush(self):
+    def crush_check(self):
+        for sprite in self.group.sprites():
+            # если объект типа Barrier и он пересекся с машиной
+            if isinstance(sprite, Barrier) and pygame.sprite.collide_rect(self, sprite):
+                sprite.kill()
+                self.barrier_crush()
+            # если объект типа TimeMoney и он пересекся с машиной
+            elif isinstance(sprite, TimeMoney) and pygame.sprite.collide_rect(self, sprite):
+                sprite.hide()
+                self.add_time()
+            # если объект типа Road и пересеклись по маске
+            elif isinstance(sprite, Road) and pygame.sprite.collide_mask(self, sprite):
+                self.road_crush()
+
+    def road_crush(self):
         play_mp3(CAR_CRASH_SOUND)
         self.stop()
         self.rect = self.rect.move(10 * -self.direct_x, 0)
@@ -107,19 +193,18 @@ class Car:
 
 
 class Road(pygame.sprite.Sprite):
-    def __init__(self, num, group, car):
+    coord_min = 271
+    coord_max = 740
+
+    def __init__(self, num, group):
         super().__init__(group)
-        self.car = car
         self.image = load_image(ROAD_IMAGE)
         self.mask = pygame.mask.from_surface(self.image)
         self.rect = self.image.get_rect()
         self.rect.y = -self.rect.h if num == 0 else 0
 
-    def update(self, *args):
-        if pygame.sprite.collide_mask(self, self.car):  # если объекты пересеклись по маске
-            self.car.side_crush()
-
-        if self.car.running:
+    def update(self, is_running, *args):
+        if is_running:
             if self.rect.y == self.rect.h - 1 or self.rect.y == -1:
                 self.rect.y -= self.rect.h - 1
             else:
@@ -127,71 +212,137 @@ class Road(pygame.sprite.Sprite):
 
 
 class Line(pygame.sprite.Sprite):
-    def __init__(self, num, group, car):
+    def __init__(self, num, group):
         super().__init__(group)
-        self.car = car
-        self.image = load_image(LINE_IMAGE)
+        self.image = pygame.Surface([LINE_WIDTH, LINE_HEIGHT])
         self.rect = self.image.get_rect()
+        pygame.draw.rect(self.image, LINE_COLOR, self.rect)
         self.rect.x = 491
         self.rect.y = num * self.rect.h * 2 - self.rect.h * 2
 
-    def update(self, *args):
-        if self.car.running:
+    def update(self, is_running, *args):
+        if is_running:
             if self.rect.y == HEIGHT:
                 self.rect.y = -self.rect.h * 2
             else:
                 self.rect = self.rect.move(0, 1)
 
 
-class BarrierBuilder:
-    road_coords = {'min': 257, 'max': 731}
-    block_width = 50
-
-    def __init__(self, group, car):
-        self.group = group
-        self.car = car
-
-        if random.randint(0, 1000) == 324:
-            self.build()
-
-    def build(self):
-        image = BarrierBuilder.get_rand_image()
-        blocks = BarrierBuilder.get_rand_blocks()
-        start_x = BarrierBuilder.get_start_x(blocks)
-
-        for i in range(blocks):
-            x = start_x + i * BarrierBuilder.block_width
-            BarrierBlock(self.group, self.car, image, x)
+class ObjectBuilder:
+    last_distance = 0
+    barrier_distance_min = 400
+    money_distance_min = 200
 
     @staticmethod
-    def get_rand_image():
-        return random.choice(BLOCK_IMAGES)
+    def build_money(group):
+        x = ObjectBuilder.get_x(TimeMoney.circle_radius * 2)
+        TimeMoney(group, x)
+
+    @staticmethod
+    def build_barrier(group):
+        colors = ObjectBuilder.get_rand_colors()
+        blocks = ObjectBuilder.get_rand_blocks()
+        start_x = ObjectBuilder.get_x(Barrier.block_width * blocks)
+        Barrier(group, colors, blocks, start_x)
+
+    @staticmethod
+    def get_rand_colors():
+        return random.choice(BARRIER_BLOCK_COLORS)
 
     @staticmethod
     def get_rand_blocks():
-        return random.randint(BLOCK_MIN, BLOCK_MAX)
+        return random.randint(BARRIER_BLOCK_MIN, BARRIER_BLOCK_MAX)
 
     @staticmethod
-    def get_start_x(blocks):
-        min = BarrierBuilder.road_coords['min']
-        max = BarrierBuilder.road_coords['max'] - BarrierBuilder.block_width * blocks
-        return random.randint(min, max)
+    def get_x(width):
+        return random.randint(Road.coord_min, Road.coord_max - width)
 
 
-class BarrierBlock(pygame.sprite.Sprite):
-    def __init__(self, group, car, image, x):
+class Barrier(pygame.sprite.Sprite):
+    block_width = 50
+    block_height = 40
+    block_border_coords = [(0, 40), (25, 0), (50, 0), (25, 40)]
+
+    def __init__(self, group, colors, blocks, x):
         super().__init__(group)
-        self.car = car
-        self.image = load_image(image)
-        self.mask = pygame.mask.from_surface(self.image)
+        self.image = pygame.Surface([Barrier.block_width * blocks, Barrier.block_height])
+        self.draw_image(colors, blocks)
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = -self.rect.h
 
-    def update(self, *args):
-        if pygame.sprite.collide_mask(self, self.car):  # если объекты пересеклись по маске
-            self.remove()
-            self.car.barrier_crush()
+    def draw_image(self, color, blocks):
+        pygame.draw.rect(self.image, color[0], self.image.get_rect())
 
-        if self.car.running:
+        for b in range(blocks):
+            pygame.draw.polygon(self.image, color[1], Barrier.get_block_coords(b))
+
+    @staticmethod
+    def get_block_coords(num):
+        coords = []
+        for c in Barrier.block_border_coords:
+            coords.append((c[0] + Barrier.block_width * num, c[1]))
+
+        return coords
+
+    def update(self, is_running, *args):
+        if is_running:
             self.rect = self.rect.move(0, 1)
+
+
+class TimeMoney(pygame.sprite.Sprite):
+    circle_radius = 35
+    circle_text = f'+{Car.timeup}'
+    circle_text_size = 35
+
+    def __init__(self, group, x):
+        super().__init__(group)
+        radius = TimeMoney.circle_radius
+        self.image = pygame.Surface([radius * 2, radius * 2])
+        self.draw_image(radius)
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = -self.rect.h
+        self.hidden = False
+
+    def draw_image(self, radius):
+        pygame.draw.circle(self.image, MONEY_COLOR, (radius, radius), radius)
+        self.image.set_colorkey((0, 0, 0))
+        font = pygame.font.SysFont(None, TimeMoney.circle_text_size)
+        text = font.render(TimeMoney.circle_text, 1, MONEY_TEXT_COLOR)
+        text_x = radius - text.get_width() // 2
+        text_y = radius - text.get_height() // 2
+        self.image.blit(text, (text_x - 2, text_y))
+
+    def hide(self):
+        self.hidden = True
+
+    def update(self, is_running, *args):
+        if self.hidden:
+            if self.rect.y + self.rect.h < 0:
+                self.kill()
+            else:
+                self.rect = self.rect.move(0, -3)
+        elif is_running:
+            self.rect = self.rect.move(0, 1)
+
+
+class Score:
+    storage = path.join(DATA_PATH, SCORE_STORAGE)
+
+    @staticmethod
+    def set(score):
+        file = open(Score.storage, 'w')
+        file.write(str(score))
+        file.close()
+
+    @staticmethod
+    def get():
+        if path.exists(Score.storage):
+            file = open(Score.storage, 'r')
+            score = int(file.readline())
+            file.close()
+        else:
+            score = 0
+
+        return score
--- main.py
+++ main.py
@@ -4,45 +4,106 @@ import pygame
 WIDTH = 1000
 HEIGHT = 1000
 
+RACE_TIME = 60
+RACE_DISTANCE_NEED = 10000
+
+ROAD_COLOR = 116, 116, 116
+LINE_COLOR = 255, 255, 255
+LINE_WIDTH = 18
+LINE_HEIGHT = 125
+
+DATA_PATH = "data"
+ROAD_IMAGE = "road.png"
+CAR_IMAGE = "car.png"
+CAR_SOUND = "car.wav"
+CAR_CRASH_SOUND = "crush.mp3"
+SCORE_STORAGE = "score.dat"
+
+WHITE_BARRIER_BLOCK = (255, 115, 0), (255, 255, 255)
+BLACK_BARRIER_BLOCK = (255, 200, 0), (0, 0, 0)
+BARRIER_BLOCK_COLORS = [WHITE_BARRIER_BLOCK, BLACK_BARRIER_BLOCK]
+BARRIER_BLOCK_MIN = 3
+BARRIER_BLOCK_MAX = 6
+BARRIER_INCIDENCE = 0.8  # 0 ... 1
+
+MONEY_COLOR = 0, 115, 255
+MONEY_TEXT_COLOR = 255, 255, 255
+MONEY_INCIDENCE = 0.6  # 0 ... 1
+
 
 def main():
     clock = pygame.time.Clock()
     pygame.init()
     screen = pygame.display.set_mode((WIDTH, HEIGHT))
-    pygame.display.set_caption('car race')
+    pygame.display.set_caption('Автогонка с препятствиями')
     pygame.mouse.set_visible(False)
 
-    sprite_group = pygame.sprite.Group()
+    all_sprites = pygame.sprite.Group()
 
-    red_car = game.Car()
-    game.Road(0, sprite_group, red_car)
-    game.Road(1, sprite_group, red_car)
+    red_car = game.Car(all_sprites)
+    game.Road(0, all_sprites)
+    game.Road(1, all_sprites)
 
     for i in range(5):
-        game.Line(i, sprite_group, red_car)
+        game.Line(i, all_sprites)
+
+    score = game.Score.get()
+    distance = 0
+    speed = 0
+    game_time = 0
+    game_begin = False
+    game_over = False
 
     while True:
-        screen.fill((116, 116, 116))
+        tick_time = clock.get_time()
+        available_time = red_car.get_available_time()
 
-        game.BarrierBuilder(sprite_group, red_car)
+        if red_car.is_running():
+            game_begin = True
 
-        sprite_group.update()
-        sprite_group.draw(screen)
+        if game_begin and not game_over:
+            game_time += tick_time / 1000
+            distance = red_car.get_distance()
 
-        red_car.update()
-        red_car.draw(screen)
+            if available_time < game_time:
+                game_over = True
 
-
-        pygame.display.flip()
+                if distance > score:
+                    game.Score.set(distance)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT:
+            if event.type == pygame.QUIT or \
+                    game_over and \
+                    event.type == pygame.KEYDOWN and \
+                    event.key == pygame.K_SPACE:
+                pygame.quit()
                 return
 
             red_car.move(event)
 
-        time = clock.get_time()
-        speed = red_car.get_speed(time)
+        screen.fill(ROAD_COLOR)
+
+        game.build_road_object(all_sprites, distance, available_time)
+
+        all_sprites.update(red_car.is_running())
+        all_sprites.draw(screen)
+
+        red_car.update(tick_time)
+        red_car.draw(screen)
+
+        game.print_stat(screen, game_time, distance, score)
+
+        if game_over:
+            if speed > 0:
+                speed -= 3
+            else:
+                red_car.stop()
+                success = distance >= RACE_DISTANCE_NEED
+                game.level_passed(screen, success)
+        else:
+            speed = red_car.get_speed()
+
+        pygame.display.flip()
         clock.tick(speed)
 
 
